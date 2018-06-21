a56ab19843c58dd74cf97fd1f4c60d4a08b137c1
smona <silbertmonaphia@gmail.com>
2018-06-21 23:10:28 +0800

    Add Commnets

--- app/controllers/Index.php
+++ app/controllers/Index.php
@@ -1,15 +1,16 @@
 <?php
 class IndexController extends Yaf_Controller_Abstract {
       public function indexAction() {
-          // getView(): fooAction get /views/foo/foo.phtml
-
+          // route args handling
           // $name = $this->getRequest()->getParam('name');
           // $val = $this->getRequest()->getParam('value');
 
+          // db manipulation
           $cursor = Database::getInstance();
           $rows = $cursor->query('SELECT role_job FROM tbllog_create;');
           $res = $rows->fetch(PDO::FETCH_ASSOC);
           foreach ($res as $content) {
+              // getView(): fooAction get /views/foo/foo.phtml
               $this->getView()->content = $content;
           }
           // self: class ,$this :object
