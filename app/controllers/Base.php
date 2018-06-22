5417e4cd3e454826cb3016f21a49cbe831da6c30
smona <zhongweijian@joygames.cc>
2018-06-22 15:22:46 +0800

    Add: turn to RESTful api

---
+++ app/controllers/Base.php
@@ -0,0 +1,19 @@
+<?php
+class BaseController extends Yaf_Controller_Abstract {
+      public function init() {
+          // Yaf_Controller_Abstract will invoke init() method automatically
+          // when it's instantialized
+          if ($this->getRequest()->isXmlHttpRequest()){
+          Yaf_Dispatcher::getInstance()->autoRender(False);
+          }
+      }
+      
+      public function __errResponseJson($errcode = -1, $errmsg = 'failed') {
+            $res = array('errcode' => $errcode, 'errmsg' => $errmsg);
+            $this->getResponse()->setBody(json_encode($res));
+      }
+
+      public function __responseJson($data = NULL, $errcode = 0, $errmsg ='success') {
+            $res = array('errcode' => $errcode, 'errmsg' => $errmsg, 'data' => $data);
+            $this->getResponse()->setBody(json_encode($res));
+      }
--- app/controllers/Index.php
+++ app/controllers/Index.php
@@ -1,6 +1,6 @@
 <?php
-class IndexController extends Yaf_Controller_Abstract {
-      public function indexAction($name, $val) {
+class IndexController extends BaseController {
+      public function indexAction() {
           // route args handling
           // DB_Demo_
           // $name = $this->getRequest()->getParam('name');
@@ -8,12 +8,14 @@ class IndexController extends Yaf_Controller_Abstract {
 
           // db manipulation
           $cursor = Database::getInstance();
-          $rows = $cursor->query('SELECT role_job FROM tbllog_create;');
-          $res = $rows->fetch(PDO::FETCH_ASSOC);
-          foreach ($res as $content) {
-              // getView(): fooAction get /views/foo/foo.phtml
-              $this->getView()->content = $content;
-          }
+          $rows = $cursor->query('SELECT count(1) FROM tbllog_create;');
+          $count = $rows->fetch(PDO::FETCH_ASSOC);
+
+          // getView(): fooAction get /views/foo/foo.phtml
+          // $this->getView()->content = $content;
+
+          return $this->__responseJson($count);
+
           // self: class ,$this :object
       }
 }
